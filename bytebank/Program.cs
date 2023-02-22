eefff56b7dc3d0f32d323d9cb860a0965044488f
Mateus Gabriel <mateus.gabriel@vocedm.com.br>
2023-02-22 15:48:55 -0300

    Iniciando em C#

---
+++ bytebank/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using bytebank.Contas;
+using bytebank.Titular;
+
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_agencia = 15;
+//contaDoAndre.conta = "1010-X";
+//contaDoAndre.saldo = 100;
+
+//Console.WriteLine("Saldo da conta do André = "+contaDoAndre.saldo);
+
+//contaDoAndre.Depositar(100);
+
+//Console.WriteLine("Saldo do André pós depósito = " + contaDoAndre.saldo);
+
+//if(contaDoAndre.Sacar(50) == true)
+//{
+//    Console.WriteLine("Saldo do André pós saque = " + contaDoAndre.saldo);
+//}
+//else
+//{
+//    Console.WriteLine("Saldo insulficiente para saque!");
+//}
+
+//ContaCorrente contaDaMaria = new ContaCorrente();
+
+//contaDaMaria.titular = "Maria Souza";
+//contaDaMaria.numero_agencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
+
+//Console.WriteLine("");
+
+//Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+
+//contaDoAndre.Transferir(50, contaDaMaria);
+
+//Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
+
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente contaDoPedro = new ContaCorrente();
+
+////////////////////////////////////////////////////
+
+//double valor = 300;
+//double valor2 = valor;
+
+//Console.WriteLine(valor != valor2);
+
+////////////////////////////////////////////////////
+
+Cliente cliente = new Cliente();
+cliente.nome = "André Silva";
+cliente.cpf = "123456789";
+cliente.profissao = "Desenvolvedor";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numero_agencia = 15;
+conta.saldo = 150;
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("Cpf = " + conta.titular.cpf);
+Console.WriteLine("Profissão = " + conta.titular.profissao);
+Console.WriteLine("N° Conta = " + conta.conta);
+Console.WriteLine("Agencia = " + conta.numero_agencia);
+Console.WriteLine("Saldo = " + conta.saldo);
+
+
+
+
+
